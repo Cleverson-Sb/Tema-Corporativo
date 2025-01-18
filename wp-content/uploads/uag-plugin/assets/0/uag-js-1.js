671864a6b3f136b1a9421a9b354116dc8e5bc3bb
Cleverson Marciel da Silva Brandão <cleversonmarcielsb@gmail.com>
2025-01-18 10:39:54 -0300

    teste

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-1.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c7e313af' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ea32790c' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-6b4ca95d' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-5b416d98' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-f395db0f' );
+});
+ });
